ff4c5ebb3abc011ea14b92c9dd8a4c2ce26c8aa4
Joshua Cearley <joshua.cearley@gmail.com>
2017-08-09 15:43:12 -0500

    Transition to SomeReal type.

--- skeasing.nim
+++ skeasing.nim
@@ -47,39 +47,39 @@ import math
 # a: amplitud
 # p: period
 
-proc linear*[T: float32|float64](t, b, c, d: T): T =
+proc linear*(t, b, c, d: SomeReal): SomeReal =
   return c * t / d + b
 
-proc inQuad*[T: float32|float64](t, b, c, d: T): T =
+proc inQuad*(t, b, c, d: SomeReal): SomeReal =
   t = t / d
   return c * pow(t, 2) + b
 
-proc outQuad*[T: float32|float64](t, b, c, d: T): T =
+proc outQuad*(t, b, c, d: SomeReal): SomeReal =
   t = t / d
   return -c * t * (t - 2) + b
 
-proc inOutQuad*[T: float32|float64](t, b, c, d: T): T =
+proc inOutQuad*(t, b, c, d: SomeReal): SomeReal =
   t = t / d * 2
   if t < 1:
     return c / 2 * pow(t, 2) + b
   else:
     return -c / 2 * ((t - 1) * (t - 3) - 1) + b
 
-proc outInQuad*[T: float32|float64](t, b, c, d: T): T =
+proc outInQuad*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return outQuad(t * 2, b, c / 2, d)
   else:
     return inQuad((t * 2) - d, b + c / 2, c / 2, d)
 
-proc inCubic*[T: float32|float64] (t, b, c, d: T): T =
+proc inCubic* (t, b, c, d: SomeReal): SomeReal =
   t = t / d
   return c * pow(t, 3) + b
 
-proc outCubic*[T: float32|float64](t, b, c, d: T): T =
+proc outCubic*(t, b, c, d: SomeReal): SomeReal =
   t = t / d - 1
   return c * (pow(t, 3) + 1) + b
 
-proc inOutCubic*[T: float32|float64](t, b, c, d: T): T =
+proc inOutCubic*(t, b, c, d: SomeReal): SomeReal =
   t = t / d * 2
   if t < 1:
     return c / 2 * t * t * t + b
@@ -87,21 +87,21 @@ proc inOutCubic*[T: float32|float64](t, b, c, d: T): T =
     t = t - 2
     return c / 2 * (t * t * t + 2) + b
 
-proc outInCubic*[T: float32|float64](t, b, c, d: T): T =
+proc outInCubic*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return outCubic(t * 2, b, c / 2, d)
   else:
     return inCubic((t * 2) - d, b + c / 2, c / 2, d)
 
-proc inQuart*[T: float32|float64](t, b, c, d: T): T =
+proc inQuart*(t, b, c, d: SomeReal): SomeReal =
   t = t / d
   return c * pow(t, 4) + b
 
-proc outQuart*[T: float32|float64](t, b, c, d: T): T =
+proc outQuart*(t, b, c, d: SomeReal): SomeReal =
   t = t / d - 1
   return -c * (pow(t, 4) - 1) + b
 
-proc inOutQuart*[T: float32|float64](t, b, c, d: T): T =
+proc inOutQuart*(t, b, c, d: SomeReal): SomeReal =
   t = t / d * 2
   if t < 1:
     return c / 2 * pow(t, 4) + b
@@ -109,21 +109,21 @@ proc inOutQuart*[T: float32|float64](t, b, c, d: T): T =
     t = t - 2
     return -c / 2 * (pow(t, 4) - 2) + b
 
-proc outInQuart*[T: float32|float64](t, b, c, d: T): T =
+proc outInQuart*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return outQuart(t * 2, b, c / 2, d)
   else:
     return inQuart((t * 2) - d, b + c / 2, c / 2, d)
 
-proc inQuint*[T: float32|float64](t, b, c, d: T): T =
+proc inQuint*(t, b, c, d: SomeReal): SomeReal =
   t = t / d
   return c * pow(t, 5) + b
 
-proc outQuint*[T: float32|float64](t, b, c, d: T): T =
+proc outQuint*(t, b, c, d: SomeReal): SomeReal =
   t = t / d - 1
   return c * (pow(t, 5) + 1) + b
 
-proc inOutQuint*[T: float32|float64](t, b, c, d: T): T =
+proc inOutQuint*(t, b, c, d: SomeReal): SomeReal =
   t = t / d * 2
   if t < 1:
     return c / 2 * pow(t, 5) + b
@@ -131,40 +131,40 @@ proc inOutQuint*[T: float32|float64](t, b, c, d: T): T =
     t = t - 2
     return c / 2 * (pow(t, 5) + 2) + b
 
-proc outInQuint*[T: float32|float64](t, b, c, d: T): T =
+proc outInQuint*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return outQuint(t * 2, b, c / 2, d)
   else:
     return inQuint((t * 2) - d, b + c / 2, c / 2, d)
 
-proc inSine*[T: float32|float64](t, b, c, d: T): T =
+proc inSine*(t, b, c, d: SomeReal): SomeReal =
   return -c * cos(t / d * (PI / 2)) + c + b
 
-proc outSine*[T: float32|float64](t, b, c, d: T): T =
+proc outSine*(t, b, c, d: SomeReal): SomeReal =
   return c * sin(t / d * (PI / 2)) + b
 
-proc inOutSine*[T: float32|float64](t, b, c, d: T): T =
+proc inOutSine*(t, b, c, d: SomeReal): SomeReal =
   return -c / 2 * (cos(PI * t / d) - 1) + b
 
-proc outInSine*[T: float32|float64](t, b, c, d: T): T =
+proc outInSine*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return outSine(t * 2, b, c / 2, d)
   else:
     return inSine((t * 2) - d, b + c / 2, c / 2, d)
 
-proc inExpo*[T: float32|float64](t, b, c, d: T): T =
+proc inExpo*(t, b, c, d: SomeReal): SomeReal =
   if t == 0:
     return b
   else:
     return c * pow(2, 10 * (t / d - 1)) + b - c * 0.001
 
-proc outExpo*[T: float32|float64](t, b, c, d: T): T =
+proc outExpo*(t, b, c, d: SomeReal): SomeReal =
   if t == d:
     return b + c
   else:
     return c * 1.001 * (-pow(2, -10 * t / d) + 1) + b
 
-proc inOutExpo*[T: float32|float64](t, b, c, d: T): T =
+proc inOutExpo*(t, b, c, d: SomeReal): SomeReal =
   t = t / d * 2
   if t < 1:
     return c / 2 * pow(2, 10 * (t - 1)) + b - c * 0.0005
@@ -172,21 +172,21 @@ proc inOutExpo*[T: float32|float64](t, b, c, d: T): T =
     t = t - 1
     return c / 2 * 1.0005 * (-pow(2, -10 * t) + 2) + b
 
-proc outInExpo*[T: float32|float64](t, b, c, d: T): T =
+proc outInExpo*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return outExpo(t * 2, b, c / 2, d)
   else:
     return inExpo((t * 2) - d, b + c / 2, c / 2, d)
 
-proc inCirc*[T: float32|float64](t, b, c, d: T): T =
+proc inCirc*(t, b, c, d: SomeReal): SomeReal =
   t = t / d
   return(-c * (sqrt(1 - pow(t, 2)) - 1) + b)
 
-proc outCirc*[T: float32|float64](t, b, c, d: T): T =
+proc outCirc*(t, b, c, d: SomeReal): SomeReal =
   t = t / d - 1
   return(c * sqrt(1 - pow(t, 2)) + b)
 
-proc inOutCirc*[T: float32|float64](t, b, c, d: T): T =
+proc inOutCirc*(t, b, c, d: SomeReal): SomeReal =
   t = t / d * 2
   if t < 1:
     return -c / 2 * (sqrt(1 - t * t) - 1) + b
@@ -194,7 +194,7 @@ proc inOutCirc*[T: float32|float64](t, b, c, d: T): T =
     t = t - 2
     return c / 2 * (sqrt(1 - t * t) + 1) + b
 
-proc outInCirc*[T: float32|float64](t, b, c, d: T): T =
+proc outInCirc*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return outCirc(t * 2, b, c / 2, d)
   else:
@@ -202,9 +202,9 @@ proc outInCirc*[T: float32|float64](t, b, c, d: T): T =
 
 # Elastic {{{
 
-proc inElastic*[T: float32|float64](t, b, c, d, a, p: T): T =
+proc inElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
   t = t / d
-  var s: T
+  var s: SomeReal
 
   if not a or a < abs(c):
     a = c
@@ -216,9 +216,9 @@ proc inElastic*[T: float32|float64](t, b, c, d, a, p: T): T =
 
   return -(a * pow(2, 10 * t) * sin((t * d - s) * (2 * PI) / p)) + b
 
-proc outElastic*[T: float32|float64](t, b, c, d, a, p: T): T =
+proc outElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
   t = t / d
-  var s: T
+  var s: SomeReal
 
   if not a or a < abs(c):
     a = c
@@ -228,9 +228,9 @@ proc outElastic*[T: float32|float64](t, b, c, d, a, p: T): T =
 
   return a * pow(2, -10 * t) * sin((t * d - s) * (2 * PI) / p) + c + b
 
-proc inOutElastic*[T: float32|float64](t, b, c, d, a, p: T): T =
+proc inOutElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
   t = t / d * 2
-  var s: T
+  var s: SomeReal
 
   if not a or a < abs(c):
     a = c
@@ -245,7 +245,7 @@ proc inOutElastic*[T: float32|float64](t, b, c, d, a, p: T): T =
     t = t - 1
     return a * pow(2, -10 * t) * sin((t * d - s) * (2 * PI) / p ) * 0.5 + c + b
 
-proc outInElastic*[T: float32|float64](t, b, c, d, a, p: T): T =
+proc outInElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
   if t < d / 2:
     return outElastic(t * 2, b, c / 2, d, a, p)
   else:
@@ -253,15 +253,15 @@ proc outInElastic*[T: float32|float64](t, b, c, d, a, p: T): T =
 
 # }}} Elastic
 
-proc inBack*[T: float32|float64](t, b, c, d, s: T): T =
+proc inBack*(t, b, c, d, s: SomeReal): SomeReal =
   t = t / d
   return c * t * t * ((s + 1) * t - s) + b
 
-proc outBack*[T: float32|float64](t, b, c, d, s: T): T =
+proc outBack*(t, b, c, d, s: SomeReal): SomeReal =
   t = t / d - 1
   return c * (t * t * ((s + 1) * t + s) + 1) + b
 
-proc inOutBack*[T: float32|float64](t, b, c, d, s: T): T =
+proc inOutBack*(t, b, c, d, s: SomeReal): SomeReal =
   s = s * 1.525
   t = t / d * 2
   if t < 1:
@@ -270,13 +270,13 @@ proc inOutBack*[T: float32|float64](t, b, c, d, s: T): T =
     t = t - 2
     return c / 2 * (t * t * ((s + 1) * t + s) + 2) + b
 
-proc outInBack*[T: float32|float64](t, b, c, d, s: T): T =
+proc outInBack*(t, b, c, d, s: SomeReal): SomeReal =
   if t < d / 2:
     return outBack(t * 2, b, c / 2, d, s)
   else:
     return inBack((t * 2) - d, b + c / 2, c / 2, d, s)
 
-proc outBounce*[T: float32|float64](t, b, c, d: T): T =
+proc outBounce*(t, b, c, d: SomeReal): SomeReal =
   t = t / d
   if t < 1 / 2.75:
     return c * (7.5625 * t * t) + b
@@ -290,16 +290,16 @@ proc outBounce*[T: float32|float64](t, b, c, d: T): T =
     t = t - (2.625 / 2.75)
     return c * (7.5625 * t * t + 0.984375) + b
 
-proc inBounce*[T: float32|float64](t, b, c, d: T): T =
+proc inBounce*(t, b, c, d: SomeReal): SomeReal =
   return c - outBounce(d - t, 0, c, d) + b
 
-proc inOutBounce*[T: float32|float64](t, b, c, d: T): T =
+proc inOutBounce*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return inBounce(t * 2, 0, c, d) * 0.5 + b
   else:
     return outBounce(t * 2 - d, 0, c, d) * 0.5 + c * 0.5 + b
 
-proc outInBounce*[T: float32|float64](t, b, c, d: T): T =
+proc outInBounce*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
     return outBounce(t * 2, b, c / 2, d)
   else:
