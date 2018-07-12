d50cda01c186818fe1e04c234feaeb3f05b1c3f4
Joshua Cearley <joshua.cearley@gmail.com>
2018-07-12 10:03:38 -0500

    Hey, most of the functions actually work now!

--- skEasing.nim
+++ skEasing.nim
@@ -51,91 +51,61 @@ proc linear*(t, b, c, d: SomeReal): SomeReal =
   return c * t / d + b
 
 proc inQuad*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d
-  return c * pow(t, 2) + b
+  let t_hat = t / d
+  return c * t_hat * t_hat + b
 
 proc outQuad*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d
-  return -c * t * (t - 2) + b
+  let t_hat = t / d
+  return -c * t_hat * (t_hat - 2) + b
 
 proc inOutQuad*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d * 2
-  if t < 1:
-    return c / 2 * pow(t, 2) + b
+  let t_hat = t / (d / 2)
+  if t_hat < 1:
+    return c / 2 * t_hat * t_hat + b
   else:
-    return -c / 2 * ((t - 1) * (t - 3) - 1) + b
-
-proc outInQuad*(t, b, c, d: SomeReal): SomeReal =
-  if t < d / 2:
-    return outQuad(t * 2, b, c / 2, d)
-  else:
-    return inQuad((t * 2) - d, b + c / 2, c / 2, d)
+    return -c / 2 * ((t_hat - 1) * (t_hat - 3) - 1) + b
 
 proc inCubic* (t, b, c, d: SomeReal): SomeReal =
-  t = t / d
-  return c * pow(t, 3) + b
+  let t_hat = t / d
+  return c * pow(t_hat, 3) + b
 
 proc outCubic*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d - 1
-  return c * (pow(t, 3) + 1) + b
+  let t_hat = t / d - 1
+  return c * (pow(t_hat, 3) + 1) + b
 
 proc inOutCubic*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d * 2
-  if t < 1:
-    return c / 2 * t * t * t + b
+  let t_hat = t / d * 2
+  if t_hat < 1:
+    return c / 2 * t_hat * t_hat * t_hat + b
   else:
-    t = t - 2
-    return c / 2 * (t * t * t + 2) + b
-
-proc outInCubic*(t, b, c, d: SomeReal): SomeReal =
-  if t < d / 2:
-    return outCubic(t * 2, b, c / 2, d)
-  else:
-    return inCubic((t * 2) - d, b + c / 2, c / 2, d)
+    let t_hat = t_hat - 2
+    return c / 2 * (t_hat * t_hat * t_hat + 2) + b
 
 proc inQuart*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d
-  return c * pow(t, 4) + b
+  return c * pow(t / d, 4) + b
 
 proc outQuart*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d - 1
-  return -c * (pow(t, 4) - 1) + b
+  return -c * (pow(t / d - 1, 4) - 1) + b
 
 proc inOutQuart*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d * 2
-  if t < 1:
-    return c / 2 * pow(t, 4) + b
+  let t_hat = t / (d / 2)
+  if t_hat < 1:
+    return c/2 * pow(t_hat, 4) + b
   else:
-    t = t - 2
-    return -c / 2 * (pow(t, 4) - 2) + b
-
-proc outInQuart*(t, b, c, d: SomeReal): SomeReal =
-  if t < d / 2:
-    return outQuart(t * 2, b, c / 2, d)
-  else:
-    return inQuart((t * 2) - d, b + c / 2, c / 2, d)
+    return -c/2 * (pow(t_hat-2, 4) - 2) + b
 
 proc inQuint*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d
-  return c * pow(t, 5) + b
+  return c * pow(t / d, 5) + b
 
 proc outQuint*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d - 1
-  return c * (pow(t, 5) + 1) + b
+  return c * (pow(t / d - 1, 5) + 1) + b
 
 proc inOutQuint*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d * 2
-  if t < 1:
-    return c / 2 * pow(t, 5) + b
+  let t_hat = t / (d / 2)
+  if t_hat < 1:
+    return c / 2 * pow(t_hat, 5) + b
   else:
-    t = t - 2
-    return c / 2 * (pow(t, 5) + 2) + b
-
-proc outInQuint*(t, b, c, d: SomeReal): SomeReal =
-  if t < d / 2:
-    return outQuint(t * 2, b, c / 2, d)
-  else:
-    return inQuint((t * 2) - d, b + c / 2, c / 2, d)
+    return c / 2 * (pow(t_hat - 2, 5) + 2) + b
 
 proc inSine*(t, b, c, d: SomeReal): SomeReal =
   return -c * cos(t / d * (PI / 2)) + c + b
@@ -145,12 +115,6 @@ proc outSine*(t, b, c, d: SomeReal): SomeReal =
 
 proc inOutSine*(t, b, c, d: SomeReal): SomeReal =
   return -c / 2 * (cos(PI * t / d) - 1) + b
-
-proc outInSine*(t, b, c, d: SomeReal): SomeReal =
-  if t < d / 2:
-    return outSine(t * 2, b, c / 2, d)
-  else:
-    return inSine((t * 2) - d, b + c / 2, c / 2, d)
 
 proc inExpo*(t, b, c, d: SomeReal): SomeReal =
   if t == 0:
@@ -165,45 +129,31 @@ proc outExpo*(t, b, c, d: SomeReal): SomeReal =
     return c * 1.001 * (-pow(2, -10 * t / d) + 1) + b
 
 proc inOutExpo*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d * 2
-  if t < 1:
-    return c / 2 * pow(2, 10 * (t - 1)) + b - c * 0.0005
-  else:
-    t = t - 1
-    return c / 2 * 1.0005 * (-pow(2, -10 * t) + 2) + b
-
-proc outInExpo*(t, b, c, d: SomeReal): SomeReal =
   if t < d / 2:
-    return outExpo(t * 2, b, c / 2, d)
-  else:
     return inExpo((t * 2) - d, b + c / 2, c / 2, d)
+  else:
+    return outExpo(t * 2, b, c / 2, d)
 
 proc inCirc*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d
-  return(-c * (sqrt(1 - pow(t, 2)) - 1) + b)
+  let t_hat = t / d
+  return(-c * (sqrt(1 - pow(t_hat, 2)) - 1) + b)
 
 proc outCirc*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d - 1
-  return(c * sqrt(1 - pow(t, 2)) + b)
+  let t_hat = t / d - 1
+  return(c * sqrt(1 - pow(t_hat, 2)) + b)
 
 proc inOutCirc*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d * 2
-  if t < 1:
-    return -c / 2 * (sqrt(1 - t * t) - 1) + b
+  let t_hat = t / d * 2
+  if t_hat < 1:
+    return -c / 2 * (sqrt(1 - t_hat * t_hat) - 1) + b
   else:
-    t = t - 2
-    return c / 2 * (sqrt(1 - t * t) + 1) + b
-
-proc outInCirc*(t, b, c, d: SomeReal): SomeReal =
-  if t < d / 2:
-    return outCirc(t * 2, b, c / 2, d)
-  else:
-    return inCirc((t * 2) - d, b + c / 2, c / 2, d)
+    let t_hat = t_hat - 2
+    return c / 2 * (sqrt(1 - t_hat * t_hat) + 1) + b
 
 # Elastic {{{
 
 proc inElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
-  t = t / d
+  let t_hat = t / d
   var s: SomeReal
 
   if not a or a < abs(c):
@@ -212,12 +162,12 @@ proc inElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
   else:
     s = p / (2 * PI) * arcsin(c/a)
 
-  t = t - 1
+  let t_hat = t - 1
 
-  return -(a * pow(2, 10 * t) * sin((t * d - s) * (2 * PI) / p)) + b
+  return -(a * pow(2, 10 * t_hat) * sin((t_hat * d - s) * (2 * PI) / p)) + b
 
 proc outElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
-  t = t / d
+  let t_hat = t / d
   var s: SomeReal
 
   if not a or a < abs(c):
@@ -226,10 +176,10 @@ proc outElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
   else:
     s = p / (2 * PI) * arcsin(c/a)
 
-  return a * pow(2, -10 * t) * sin((t * d - s) * (2 * PI) / p) + c + b
+  return a * pow(2, -10 * t_hat) * sin((t_hat * d - s) * (2 * PI) / p) + c + b
 
 proc inOutElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
-  t = t / d * 2
+  let t_hat = t / d * 2
   var s: SomeReal
 
   if not a or a < abs(c):
@@ -239,11 +189,11 @@ proc inOutElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
     s = p / (2 * PI) * arcsin(c / a)
 
   if t < 1:
-    t = t - 1
-    return -0.5 * (a * pow(2, 10 * t) * sin((t * d - s) * (2 * PI) / p)) + b
+    let t_hat = t - 1
+    return -0.5 * (a * pow(2, 10 * t_hat) * sin((t_hat * d - s) * (2 * PI) / p)) + b
   else:
-    t = t - 1
-    return a * pow(2, -10 * t) * sin((t * d - s) * (2 * PI) / p ) * 0.5 + c + b
+    let t_hat = t - 1
+    return a * pow(2, -10 * t_hat) * sin((t_hat * d - s) * (2 * PI) / p ) * 0.5 + c + b
 
 proc outInElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
   if t < d / 2:
@@ -254,21 +204,21 @@ proc outInElastic*(t, b, c, d, a, p: SomeReal): SomeReal =
 # }}} Elastic
 
 proc inBack*(t, b, c, d, s: SomeReal): SomeReal =
-  t = t / d
-  return c * t * t * ((s + 1) * t - s) + b
+  let t_hat = t / d
+  return c * t_hat * t_hat * ((s + 1) * t_hat - s) + b
 
 proc outBack*(t, b, c, d, s: SomeReal): SomeReal =
-  t = t / d - 1
-  return c * (t * t * ((s + 1) * t + s) + 1) + b
+  let t_hat = t / d - 1
+  return c * (t_hat * t_hat * ((s + 1) * t_hat + s) + 1) + b
 
 proc inOutBack*(t, b, c, d, s: SomeReal): SomeReal =
   s = s * 1.525
-  t = t / d * 2
+  let t_hat = t / d * 2
   if t < 1:
-    return c / 2 * (t * t * ((s + 1) * t - s)) + b
+    return c / 2 * (t_hat * t_hat * ((s + 1) * t_hat - s)) + b
   else:
-    t = t - 2
-    return c / 2 * (t * t * ((s + 1) * t + s) + 2) + b
+    let t_hat = t - 2
+    return c / 2 * (t_hat * t_hat * ((s + 1) * t_hat + s) + 2) + b
 
 proc outInBack*(t, b, c, d, s: SomeReal): SomeReal =
   if t < d / 2:
@@ -277,18 +227,18 @@ proc outInBack*(t, b, c, d, s: SomeReal): SomeReal =
     return inBack((t * 2) - d, b + c / 2, c / 2, d, s)
 
 proc outBounce*(t, b, c, d: SomeReal): SomeReal =
-  t = t / d
+  let t_hat = t / d
   if t < 1 / 2.75:
-    return c * (7.5625 * t * t) + b
+    return c * (7.5625 * t_hat * t_hat) + b
   elif t < 2 / 2.75:
-    t = t - (1.5 / 2.75)
-    return c * (7.5625 * t * t + 0.75) + b
+    let t_hat = t - (1.5 / 2.75)
+    return c * (7.5625 * t_hat * t_hat + 0.75) + b
   elif t < 2.5 / 2.75:
-    t = t - (2.25 / 2.75)
-    return c * (7.5625 * t * t + 0.9375) + b
+    let t_hat = t - (2.25 / 2.75)
+    return c * (7.5625 * t_hat * t_hat + 0.9375) + b
   else:
-    t = t - (2.625 / 2.75)
-    return c * (7.5625 * t * t + 0.984375) + b
+    let t_hat = t - (2.625 / 2.75)
+    return c * (7.5625 * t_hat * t_hat + 0.984375) + b
 
 proc inBounce*(t, b, c, d: SomeReal): SomeReal =
   return c - outBounce(d - t, 0, c, d) + b
