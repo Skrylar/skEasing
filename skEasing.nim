9c369849c65d9425eab698bd578922d7f360f330
Joshua Cearley <joshua.cearley@gmail.com>
2018-07-12 11:58:30 -0500

    Juckett's damped spring functions.

--- skEasing.nim
+++ skEasing.nim
@@ -37,7 +37,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import math
+import math, fenv
 
 # For all easing functions:
 # t = elapsed time
@@ -147,3 +147,109 @@ proc inOutCirc*(t, b, c, d: SomeReal): SomeReal =
   else:
     let t_hat = t_hat - 2
     return c / 2 * (sqrt(1 - t_hat * t_hat) + 1) + b
+
+# Damped spring motion
+
+#  Copyright (c) 2008-2012 Ryan Juckett http://www.ryanjuckett.com/
+#
+#  This software is provided 'as-is', without any express or implied
+#  warranty. In no event will the authors be held liable for any
+#  damages arising from the use of this software.
+#
+#  Permission is granted to anyone to use this software for any purpose,
+#  including commercial applications, and to alter it and redistribute
+#  it freely, subject to the following restrictions:
+#
+#  1. The origin of this software must not be misrepresented; you
+#  must not
+#     claim that you wrote the original software. If you use this
+#     software in a product, an acknowledgment in the product
+#     documentation would be appreciated but is not required.
+#
+#  2. Altered source versions must be plainly marked as such, and must
+#  not be
+#     misrepresented as being the original software.
+#
+#  3. This notice may not be removed or altered from any source
+#     distribution.
+
+# Sk: Consider the following code an 'altered source version' in
+# its entirety
+
+type
+  DampedSpringCoefficients*[T:SomeReal] = object
+    ## Caches coefficients so that multiple springs with
+    ## the same parameters may be used without re-running
+    ## trigonometry calls.
+    posPosCoef, posVelCoef: T
+    velPosCoef, velVelCoef: T
+
+proc init*[T:SomeReal] (self: var DampedSpringCoefficients[T];
+            delta_time,
+            desired_angular_frequency,
+            desired_damping_ratio: T) =
+  ## Performs (somewhat nontrivial) calculations to prepare
+  ## the motion of a damped spring. Once initialized, you
+  ## may update any spring with identical properties with
+  ## `update` using the same object.
+
+  # clamping
+  let damping_ratio = if desired_damping_ratio < 0.0: 0.0 else: desired_damping_ratio
+  let angular_frequency = if desired_angular_frequency < 0.0: 0.0 else: desired_angular_frequency
+
+  # special case: no angular frequency
+  if angular_frequency < epsilon(T):
+    self.posPosCoef = 1.0
+    self.posVelCoef = 0.0
+    self.velPosCoef = 0.0
+    self.velVelCoef = 1.0
+    return
+
+  if damping_ratio > 1.0 + epsilon(T): # over-damped
+    let za = -angular_frequency * damping_ratio
+    let zb = angular_frequency * sqrt(damping_ratio * damping_ratio - 1.0)
+    let z1 = za - zb
+    let z2 = za + zb
+    let e1 = exp(z1 * delta_time)
+    let e2 = exp(z2 * delta_time)
+    let inv_two_zb = 1.0 / (2.0 * zb)
+    let e1_over_twozb = e1 * inv_two_zb
+    let e2_over_twozb = e2 * inv_two_zb
+    let z1e1_over_twozb = z1 * e1_over_twozb
+    let z2e2_over_twozb = z2 * e2_over_twozb
+    self.posPosCoef = e1_over_twozb * z2 - z2e2_over_twozb + e2
+    self.posVelCoef = -e1_over_twozb + e2_over_twozb
+    self.velPosCoef = (z1e1_over_twozb - z2e2_over_twozb + e2) * z2
+    self.velVelCoef = -z1e1_over_twozb + z2e2_over_twozb
+  elif damping_ratio < 1.0 - epsilon(T): # under-damped
+    let omega_zeta = angular_frequency * damping_ratio
+    let alpha = angular_frequency * sqrt(1.0 - damping_ratio * damping_ratio)
+    let exp_term = exp(-omega_zeta * delta_time)
+    let cos_term = cos(alpha * delta_time)
+    let sin_term = sin(alpha * delta_time)
+    let inv_alpha = 1.0 / alpha
+    let exp_sin = exp_term * sin_term
+    let exp_cos = exp_term * cos_term
+    let exp_omega_zeta_sin_over_alpha = exp_term * omega_zeta * sin_term * inv_alpha
+    self.posPosCoef = exp_cos + exp_omega_zeta_sin_over_alpha
+    self.posVelCoef = exp_sin * inv_alpha
+    self.velPosCoef = -exp_sin * alpha - omega_zeta * exp_omega_zeta_sin_over_alpha
+    self.velVelCoef = exp_cos - exp_omega_zeta_sin_over_alpha
+  else: # critically damped
+    let exp_term = exp(-angular_frequency * delta_time)
+    let time_exp = delta_time * exp_term
+    let time_exp_freq = time_exp * angular_frequency
+    self.posPosCoef = time_exp_freq + exp_term
+    self.posVelCoef = time_exp
+    self.velPosCoef = -angular_frequency * time_exp_freq
+    self.velVelCoef = -time_exp_freq + exp_term
+
+# XXX is there a performance implication to not using ref/var here?
+proc update* [T: SomeReal] (self: DampedSpringCoefficients[T];
+              position, velocity: var T;
+              target: T) =
+  let old_pos = position - target
+  let old_vel = velocity
+  position = old_pos * self.posPosCoef + old_vel * self.posVelCoef + target
+  velocity = old_pos * self.velPosCoef + old_vel * self.velVelCoef
+
